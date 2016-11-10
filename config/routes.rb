9856fb926a4eab8d2e2b87ab0d15ab885b86eb57
at-quoctv <at-quoctv@asiantech.vn>
2016-11-10 11:11:07 +0700

    finish user,update,index

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get  '/login',  to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' 
+
+  get '/users', to: 'users#index'
   # get  '/users',  to:'users#index'
   resources :users
 end
